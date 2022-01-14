68e40dc9fda7b49cd91dca58e9f01cb9ea005dfd
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-01-13 21:25:26 -0300

    chore: remove other value in last variable

--- src/multi_account.py
+++ src/multi_account.py
@@ -77,11 +77,7 @@ class MultiAccount:
     def botSingleOnlyMap(self):
 
         last = {
-            "login": 0,
-            "heroes": 0,
             "new_map": 0,
-            "refresh_heroes": 0,
-            "check_updates": 0
         }
 
         while True:
