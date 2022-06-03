e87a1577d88569f0892890b22badf99e6d367d16
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-06-03 18:49:58 -0300

    fix: fix the home button

--- src/heroes.py
+++ src/heroes.py
@@ -201,14 +201,14 @@ class Heroes:
         return False
 
     def sendToHome(self, rarities, bar, buttons):
-        y = bar[1]
-        for (_, rarity_y, _, rarity_h) in reversed(rarities):
-            isRariryBelow = y < (rarity_y + rarity_h)
-            isRarityAbove = y > (rarity_y - rarity_h)
+        bar_axis_y = bar[1]
+        for (_, rarity_y, _, rarity_h) in (rarities):
+            isRariryBelow = bar_axis_y < (rarity_y + rarity_h)
+            isRarityAbove = (bar_axis_y+rarity_h) > (rarity_y - rarity_h)
             if isRariryBelow and isRarityAbove:
-                for (_, button_y, _, button_h) in reversed(buttons):
-                    isBelow = y < (button_y + button_h)
-                    isAbove = y > (button_y - button_h)
+                for (_, button_y, _, button_h) in (buttons):
+                    isBelow = bar_axis_y < (button_y + button_h)
+                    isAbove = bar_axis_y > (button_y - button_h)
                     if isBelow and isAbove:
                         return True
 
@@ -367,7 +367,7 @@ class Heroes:
                              len(homeButtons), emoji='🔳', color='red')
 
         red_bars = []
-        for bar in reversed(bar_red_elements):
+        for bar in (bar_red_elements):
             sendHome = self.sendToHome(rarities, bar, homeButtons)
             if sendHome is True:
                 red_bars.append(bar)
